58ed7292e4cf013dfd7aa73f1dab082657364503
prbartman <prbartman@gmail.com>
2021-03-26 15:11:36 +0100

    initial import

---
+++ tests/test_run_notebooks.py
@@ -0,0 +1,28 @@
+import nbformat
+import pytest
+import pathlib
+import re
+import os
+from nbconvert.preprocessors import ExecutePreprocessor
+
+# https://stackoverflow.com/questions/7012921/recursive-grep-using-python
+def findfiles(path, regex):
+    regObj = re.compile(regex)
+    res = []
+    for root, dirs, fnames in os.walk(path):
+        for fname in fnames:
+            if regObj.match(fname):
+                res.append(os.path.join(root, fname))
+    return res
+
+
+@pytest.fixture(params=findfiles(pathlib.Path(__file__).parent.parent.absolute(), r'.*\.(ipynb)$'))
+def notebook_filename(request):
+    return request.param
+
+
+def test_run_notebooks(notebook_filename):
+    with open(notebook_filename) as f:
+        nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+        ep.preprocess(nb, {'metadata': {'path': '.'}})
