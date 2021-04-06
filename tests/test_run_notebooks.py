61179e4213153725c3311c61aea399af3f563c01
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-04-06 15:44:41 +0200

    add __ini__.py to B_et_al_21 example; run notebooks in temp path

--- tests/test_run_notebooks.py
+++ tests/test_run_notebooks.py
@@ -27,8 +27,8 @@ def notebook_filename(request):
     return request.param
 
 
-def test_run_notebooks(notebook_filename):
+def test_run_notebooks(notebook_filename, tmp_path):
     with open(notebook_filename, encoding="utf8") as f:
         nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=15*60, kernel_name='python3')
-        ep.preprocess(nb, {'metadata': {'path': os.path.dirname(notebook_filename)}})
+        ep.preprocess(nb, {'metadata': {'path': tmp_path}})
