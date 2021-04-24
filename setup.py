1d611f46a95c3859b80342dc011aa6737f9d114f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-04-24 11:24:21 +0200

    bump PySDM req

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PySDM-examples',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    install_requires=['PySDM @ git+https://github.com/slayoo/PySDM@c960b79#egg=PySDM',
+    install_requires=['PySDM @ git+https://github.com/slayoo/PySDM@76554b2#egg=PySDM',
                       'PyMPDATA @ git+https://github.com/atmos-cloud-sim-uj/PyMPDATA@f33d602#egg=PyMPDATA',
                       'pystrict>=1.1',
                       'matplotlib>=3.2.2',
