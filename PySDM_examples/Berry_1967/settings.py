5a3dfee48086f2de50c3d176eba897211b3fea31
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-04-29 18:47:22 +0200

    fixing Berry settings (volume)

--- PySDM_examples/Berry_1967/settings.py
+++ PySDM_examples/Berry_1967/settings.py
@@ -5,20 +5,20 @@ Created at 08.08.2019
 import numpy as np
 from PySDM.initialisation.spectra import Exponential
 from PySDM.physics.coalescence_kernels import Geometric
-from PySDM.physics.constants import si
-from PySDM.physics import formulae as phys
+from PySDM.physics import si, Formulae
 from pystrict import strict
 
 
 @strict
 class Settings:
     def __init__(self):
-        self.init_x_min = phys.volume(radius=3.94 * si.micrometre)
-        self.init_x_max = phys.volume(radius=25 * si.micrometres)
+        self.formulae = Formulae()
+        self.init_x_min = self.formulae.trivia.volume(radius=3.94 * si.micrometre)
+        self.init_x_max = self.formulae.trivia.volume(radius=25 * si.micrometres)
 
         self.n_sd = 2 ** 13
         self.n_part = 239 / si.cm**3
-        self.X0 = phys.volume(radius=10 * si.micrometres)
+        self.X0 = self.formulae.trivia.volume(radius=10 * si.micrometres)
         self.dv = 1e1 * si.metres**3  # TODO #336 1e6 do not work with ThrustRTC (overflow?)
         self.norm_factor = self.n_part * self.dv
         self.rho = 1000 * si.kilogram / si.metre**3
