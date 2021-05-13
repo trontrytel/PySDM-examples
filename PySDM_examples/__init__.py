54e6a2fe94379a06700c61bcb3e60bf5396e12aa
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-05-13 19:55:21 +0200

    clever __version__ setting in __init__.py

--- PySDM_examples/__init__.py
+++ PySDM_examples/__init__.py
@@ -1,3 +1,10 @@
 """
 Created at 08.08.2019
 """
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
