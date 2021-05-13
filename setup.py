bf8ad09374befac56e7b62cb7a4192be6df145cc
Anna Jaruga <ajaruga@caltech.edu>
2021-05-12 17:57:59 -0700

    take setup requirements from my repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='PySDM-examples',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    install_requires=['PySDM @ git+https://github.com/atmos-cloud-sim-uj/PySDM@c3b2637#egg=PySDM',
+    install_requires=['PySDM @ git+https://github.com/trontrytel/PySDM@78c9aab#egg=PySDM',
                       'PyMPDATA @ git+https://github.com/atmos-cloud-sim-uj/PyMPDATA@f33d602#egg=PyMPDATA',
                       'pystrict>=1.1',
                       'matplotlib>=3.2.2',
