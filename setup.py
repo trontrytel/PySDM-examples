23c4e18a99160edc894aa3d3a5d06602f3eb7193
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-05-14 11:03:47 +0200

    bump PyMPDATA requirement again

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     install_requires=['PySDM @ git+https://github.com/atmos-cloud-sim-uj/PySDM@c3b2637#egg=PySDM',
-                      'PyMPDATA @ git+https://github.com/atmos-cloud-sim-uj/PyMPDATA@8e06399#egg=PyMPDATA',
+                      'PyMPDATA @ git+https://github.com/atmos-cloud-sim-uj/PyMPDATA@46f6d46#egg=PyMPDATA',
                       'pystrict>=1.1',
                       'matplotlib>=3.2.2',
                       'ipywidgets>=7.5.1',
