a0fda51127dccea6d6b640e115229e6951c3a1f8
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-05-17 20:14:18 +0200

    skip setting __version__ if there is a VersionConflict

--- PySDM_examples/__init__.py
+++ PySDM_examples/__init__.py
@@ -2,9 +2,9 @@
 Created at 08.08.2019
 """
 
-from pkg_resources import get_distribution, DistributionNotFound
+from pkg_resources import get_distribution, DistributionNotFound, VersionConflict
 try:
     __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+except (DistributionNotFound, VersionConflict):
     # package is not installed
     pass
