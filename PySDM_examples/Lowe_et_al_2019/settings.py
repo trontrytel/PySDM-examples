1d1b47b2ec2ae71dc402a579f97cee8ded3beb61
claresinger <csinger@caltech.edu>
2021-06-17 14:16:42 -0700

    first attempt wip of Lowe et al 2019 example for surface partitioning of organic species

---
+++ PySDM_examples/Lowe_et_al_2019/settings.py
@@ -0,0 +1,59 @@
+import numpy as np
+from pystrict import strict
+from PySDM.initialisation import spectral_sampling as spec_sampling
+from PySDM.physics import si, Formulae, spectra, constants as const
+
+
+@strict
+class Settings:
+    def __init__(self, dt: float, n_sd: int, n_substep: int,
+                 spectral_sampling: spec_sampling.SpectralSampling = spec_sampling.Logarithmic):
+        self.formulae = Formulae(saturation_vapour_pressure='AugustRocheMagnus')
+
+        self.system_type = 'closed'
+
+        self.t_max = (2400 + 196) * si.s
+        self.output_interval = 10 * si.s
+        self.dt = dt
+
+        self.w = .5 * si.m / si.s
+        self.g = 10 * si.m / si.s**2
+
+        self.n_sd = n_sd
+        self.n_substep = n_substep
+
+        self.p0 = 950 * si.mbar
+        self.T0 = 285.2 * si.K
+        pv0 = .95 * self.formulae.saturation_vapour_pressure.pvs_Celsius(self.T0 - const.T0)
+        self.q0 = const.eps * pv0 / (self.p0 - pv0)
+        self.kappa = .61
+
+        self.cloud_radius_range = (
+                .5 * si.micrometre,
+                25 * si.micrometre
+        )
+
+        self.mass_of_dry_air = 44
+
+        # note: rho is not specified in the paper
+        rho0 = 1
+
+        self.r_dry, self.n_in_dv = spectral_sampling(
+            spectrum=spectra.Lognormal(
+                norm_factor=566 / si.cm**3 / rho0 * self.mass_of_dry_air,
+                m_mode=.08 * si.um / 2,
+                s_geom=2
+            )
+        ).sample(n_sd)
+        
+        self.dry_radius_bins_edges = np.logspace(np.log10(.01 * si.um), np.log10(1 * si.um), 51, endpoint=True) / 2
+
+    @property
+    def nt(self):
+        nt = self.t_max / self.dt
+        assert nt == int(nt)
+        return int(nt)
+
+    @property
+    def steps_per_output_interval(self) -> int:
+        return int(self.output_interval / self.dt)
---
+++ PySDM_examples/Lowe_et_al_2019/simulation.py
@@ -0,0 +1,57 @@
+from PySDM.environments import Parcel
+from PySDM import Builder
+from PySDM.backends import CPU
+from PySDM.physics import si
+from PySDM.dynamics import AmbientThermodynamics, Condensation
+import PySDM.products as PySDM_products
+import numpy as np
+
+
+class Simulation:
+    def __init__(self, settings, products=None):
+        env = Parcel(dt=settings.dt, mass_of_dry_air=settings.mass_of_dry_air, p0=settings.p0, q0=settings.q0,
+                     T0=settings.T0, w=settings.w, g=settings.g)
+
+        builder = Builder(n_sd=settings.n_sd, backend=CPU, formulae=settings.formulae)
+        builder.set_environment(env)
+
+        attributes = env.init_attributes(
+            n_in_dv=settings.n_in_dv,
+            kappa=settings.kappa,
+            r_dry=settings.r_dry
+        )
+
+        builder.add_dynamic(AmbientThermodynamics())
+        builder.add_dynamic(Condensation(kappa=settings.kappa))
+
+        products = products or (
+            PySDM_products.RelativeHumidity(),
+            PySDM_products.WaterMixingRatio(name='ql', description_prefix='liquid', radius_range=[1*si.um, np.inf]),
+            PySDM_products.ParcelDisplacement(),
+            PySDM_products.Pressure(),
+            PySDM_products.Temperature(),
+            PySDM_products.DryAirDensity(),
+            PySDM_products.WaterVapourMixingRatio(),
+            PySDM_products.Time(),
+            PySDM_products.TotalDryMassMixingRatio(settings.DRY_RHO),
+            PySDM_products.PeakSupersaturation(),
+            PySDM_products.CloudDropletConcentration(radius_range=settings.cloud_radius_range),
+        )
+
+        self.core = builder.build(attributes=attributes, products=products)
+        self.settings = settings
+
+    def _save(self, output):
+        for k, v in self.core.products.items():
+            value = v.get()
+            if isinstance(value, np.ndarray) and value.size == 1:
+                value = value[0]
+            output[k].append(value)
+
+    def run(self):
+        output = {k: [] for k in self.core.products.keys()}
+        self._save(output)
+        for _ in range(0, self.settings.nt+1, self.settings.steps_per_output_interval):
+            self.core.run(steps=self.settings.steps_per_output_interval)
+            self._save(output)
+        return output
