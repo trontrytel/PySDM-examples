322312aa2b454c827e8b59e736757b9c162c3429
piotrbartman <prbartman@gmail.com>
2021-06-25 12:22:31 +0200

    get 0 cell if first dimension equals 1

--- PySDM_examples/Kreidenweis_et_al_2003/simulation.py
+++ PySDM_examples/Kreidenweis_et_al_2003/simulation.py
@@ -60,7 +60,7 @@ class Simulation:
     def _save(self, output):
         for k, v in self.core.products.items():
             value = v.get()
-            if isinstance(value, np.ndarray) and value.size == 1:
+            if isinstance(value, np.ndarray) and value.shape[0] == 1:
                 value = value[0]
             output[k].append(value)
 
