d58bd369e430f7af2730e4ef3882a988c170ee22
Anna Jaruga <ajaruga@caltech.edu>
2021-08-17 17:28:31 -0700

    wip on Abdul-Razzak and Ghan comparison

--- PySDM_examples/SURF_activation/settings.py
+++ PySDM_examples/SURF_activation/settings.py
@@ -10,23 +10,32 @@ from pystrict import strict
 class Settings:
 
     def __init__(self, n_sd: int = 100, dt_output: float = 1 * si.second, dt_max: float = 1 * si.second):
-        self.total_time = 3 * si.hours
+
+        self.total_time = 15 * si.minutes #3 * si.hours
         self.mass_of_dry_air = 1000 * si.kilogram  # TODO #335 doubled with jupyter si unit
 
         self.n_steps = int(self.total_time / (5 * si.second))  # TODO #334 rename to n_output
         self.n_sd = n_sd
+
+        self.mode_1 = spectra.Lognormal(
+            norm_factor=1000 / si.milligram * self.mass_of_dry_air,
+            m_mode=0.04 * si.micrometre,
+            s_geom=1.4
+        )
+        self.mode_2 = spectra.Lognormal(
+          norm_factor=1000 / si.milligram * self.mass_of_dry_air,
+          m_mode=0.15 * si.micrometre,
+          s_geom=1.6
+        )
         self.r_dry, self.n = spectral_sampling.Logarithmic(
-            spectrum=spectra.Lognormal(
-                norm_factor=1000 / si.milligram * self.mass_of_dry_air,
-                m_mode=50 * si.nanometre,
-                s_geom=1.4
-            ),
+            spectrum=spectra.Sum((self.mode_1, self.mode_2)),
             size_range=(10.633 * si.nanometre, 513.06 * si.nanometre)
         ).sample(n_sd)
+
         self.dt_max = dt_max
 
         self.dt_output = dt_output
-        self.r_bins_edges = np.linspace(0 * si.micrometre, 20 * si.micrometre, 101, endpoint=True)
+        self.r_bins_edges = np.linspace(0 * si.micrometre, 10 * si.micrometre, 1001, endpoint=True)
 
         self.backend = CPU
         self.coord = 'VolumeLogarithm'
@@ -44,6 +53,4 @@ class Settings:
         self.t0 = 1200 * si.second
         self.f0 = 1 / 1000 * si.hertz
 
-    def w(self, t):
-        return .5 * (np.where(t < self.t0, 1, np.sign(-np.sin(2 * np.pi * self.f0 * (t - self.t0))))) \
-               * si.metre / si.second
+        self.w = 0.5 *si.metre / si.second
