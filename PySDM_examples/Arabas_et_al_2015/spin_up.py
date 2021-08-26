18ca0368bde3ab819dc945b47090ac0fa03773f3
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-08-25 23:28:35 -0500

    kinematic_2D examples cleanup and GUI beef up; option to plot freezing temp spectrum

--- PySDM_examples/Arabas_et_al_2015/spin_up.py
+++ PySDM_examples/Arabas_et_al_2015/spin_up.py
@@ -1,6 +1,4 @@
-from PySDM.dynamics import Displacement
-from PySDM.dynamics import Coalescence
-import warnings
+from PySDM.dynamics import Displacement, Coalescence, Freezing
 
 
 class SpinUp:
@@ -11,15 +9,15 @@ class SpinUp:
         self.core = core
         self.set(Coalescence, 'enable', False)
         self.set(Displacement, 'enable_sedimentation', False)
+        self.set(Freezing, 'enable', False)
 
     def notify(self):
         if self.core.n_steps == self.spin_up_steps:
             self.set(Coalescence, 'enable', True)
             self.set(Displacement, 'enable_sedimentation', True)
+            self.set(Freezing, 'enable', True)
 
     def set(self, dynamic, attr, value):
         key = dynamic.__name__
         if key in self.core.dynamics:
             setattr(self.core.dynamics[key], attr, value)
-        else:
-            warnings.warn(f"{key} not found!")
