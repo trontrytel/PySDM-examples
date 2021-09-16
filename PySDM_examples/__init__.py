a204be990d9579e0aaeea1c52e2896d80619e5e5
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-09-16 13:08:26 -0500

    introducing pip_install_on_colab()

--- PySDM_examples/__init__.py
+++ PySDM_examples/__init__.py
@@ -1,7 +1,3 @@
-"""
-Created at 08.08.2019
-"""
-
 from pkg_resources import get_distribution, DistributionNotFound, VersionConflict
 try:
     __version__ = get_distribution(__name__).version
--- PySDM_examples/utils/__init__.py
+++ PySDM_examples/utils/__init__.py
@@ -2,3 +2,4 @@ from .dummy_controller import DummyController
 from .temporary_file import TemporaryFile
 from .show_plot import show_plot
 from .progbar_controller import ProgBarController
+from .pip_install_on_colab import pip_install_on_colab
