69753ec6f465a2b78848774e7b2b6b7889573fb1
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-12-14 09:33:13 -0600

    renaming _Aerosol to Aerosol to avoid pdoc warning

--- PySDM_examples/Lowe_et_al_2019/settings.py
+++ PySDM_examples/Lowe_et_al_2019/settings.py
@@ -3,7 +3,7 @@ from pystrict import strict
 from PySDM import Formulae
 from PySDM.initialisation.sampling import spectral_sampling as spec_sampling
 from PySDM.physics import si, constants as const
-from PySDM_examples.Lowe_et_al_2019.aerosol import _Aerosol
+from PySDM_examples.Lowe_et_al_2019.aerosol import Aerosol
 
 
 @strict
