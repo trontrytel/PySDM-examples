0806ab022e9eabf83689b7d8c13a022b63de8b5a
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2021-12-20 13:49:13 -0600

    cleanups around rhod_wmax in ICMW examples

--- PySDM_examples/Arabas_et_al_2015/settings.py
+++ PySDM_examples/Arabas_et_al_2015/settings.py
@@ -1,21 +1,23 @@
 from typing import Iterable
-from pystrict import strict
 from PySDM.physics import si
 from PySDM.backends.impl_numba.conf import JIT_FLAGS
 from PySDM_examples.Morrison_and_Grabowski_2007.strato_cumulus import StratoCumulus
 
 
-@strict
 class Settings(StratoCumulus):
     def __dir__(self) -> Iterable[str]:
         return 'dt', 'grid', 'size', 'n_spin_up', 'versions', 'steps_per_output_interval',\
                'formulae', 'initial_dry_potential_temperature_profile',\
-               'initial_vapour_mixing_ratio_profile'
+               'initial_vapour_mixing_ratio_profile', 'rho_w_max'
 
-    def __init__(self, fastmath: bool = JIT_FLAGS['fastmath']):
+    def __init__(
+        self,
+        fastmath: bool = JIT_FLAGS['fastmath'],
+        rhod_w_max: float = .6 * si.metres / si.seconds * (si.kilogram / si.metre ** 3)
+    ):
         super().__init__(
             fastmath,
-            rho_w_max=.6 * si.metres / si.seconds * (si.kilogram / si.metre ** 3)
+            rhod_w_max=rhod_w_max
         )
 
         self.grid = (25, 25)
